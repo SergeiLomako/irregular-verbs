d1d9b8bf2e1d4546ed77e1119206efb7fa519c04
SergeiLomako <flameseeker23@gmail.com>
2019-10-30 16:33:15 +0200

    Updates

---
+++ precache-manifest.c138d5171788d0e5742fb1c2e4e58f73.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1ae68d867b0e5fe1abfcbc5aab595395",
+    "url": "/irregular-verbs/index.html"
+  },
+  {
+    "revision": "06f502dcbba47cb0ef78",
+    "url": "/irregular-verbs/static/css/main.bfd6aad1.chunk.css"
+  },
+  {
+    "revision": "577aae38a63deb8600e6",
+    "url": "/irregular-verbs/static/js/2.83ce1f31.chunk.js"
+  },
+  {
+    "revision": "06f502dcbba47cb0ef78",
+    "url": "/irregular-verbs/static/js/main.d042524f.chunk.js"
+  },
+  {
+    "revision": "73fc042b94d879f66240",
+    "url": "/irregular-verbs/static/js/runtime-main.579a62ad.js"
+  }
+]);
